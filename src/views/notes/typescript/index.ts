c52cfff7eb2719abd68f9c2f591a9b997ec23dae
kajiecy <851416347@qq.com>
2019-02-15 13:53:28 +0800

    结构调整

---
+++ src/views/notes/typescript/index.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+let name: string = `Gene`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${ name }.
+I'll be ${ age + 1 } years old next month.`;
+
+let list: Array<number> = [1, 2, 3];
