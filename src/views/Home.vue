ce9c904d2a98c03f7e6599920a91fae434a8ad7f
kajiecy <851416347@qq.com>
2019-02-18 11:34:49 +0800

    在vue-cli中使用typeScript

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" v-model="clickCount"/>
+    显示文本：{{showCountTest}}<br>
+    <input type="text" v-model="clickCount"/>
+    <button @click="addClickCount">点击</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue,Watch,Emit,Provide } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
@@ -14,5 +17,26 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    @Provide('users')
+    users = [
+        {
+            name: 'test',
+            id: 0
+        }
+    ]
+
+    clickCount:number = 0;
+    addClickCount(){
+        this.clickCount ++;
+        this.users[0].id++;
+    }
+    @Watch('clickCount', {deep: true})
+    watchCount(newVal, oldVal) {
+        console.log("newVal", newVal, "oldVal", oldVal)
+    }
+    get showCountTest(){
+        return `${this.clickCount}次`
+    }
+}
 </script>
